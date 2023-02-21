a52c2ae620747e6b72db3edb02618087a41c7d84
doldrums <doldrums.317@gmail.com>
2023-02-21 22:15:11 +0300

    Initial Commit

---
+++ VitaminReminder/VitaminReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaminReminderApp.swift
+//  VitaminReminder
+//
+//  Created by Orina on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct VitaminReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
